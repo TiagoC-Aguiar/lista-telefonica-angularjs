f91b8c7e3a289f7c4f965abf6b267d421b142ff5
Tiago Aguiar <tyago_tka48@hotmail.com>
2020-12-27 20:35:27 -0300

    adicionado eslint e editorconfig ao projeto

--- frontend/js/controllers/listaTelefonicaCtrl.js
+++ frontend/js/controllers/listaTelefonicaCtrl.js
@@ -1,46 +1,46 @@
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, contatosAPI, operadorasAPI, serialGenerator) {
-    $scope.app = "Lista Telefônica";
-    $scope.contatos = [];
-    $scope.operadoras = [];
+angular.module('listaTelefonica').controller('listaTelefonicaCtrl', ($scope, contatosAPI, operadorasAPI, serialGenerator) => {
+  $scope.app = 'Lista Telefônica';
+  $scope.contatos = [];
+  $scope.operadoras = [];
 
-    let carregarContatos = function () {
-        contatosAPI.getContatos().then(function (response) {
-            $scope.contatos = response.data;
-        }).catch(function (error) {
-            $scope.message = "Erro ao carregar contatos: " + error.data;
-        });
-    };
+  const carregarContatos = () => {
+    contatosAPI.getContatos().then(function (response) {
+      $scope.contatos = response.data;
+    }).catch(function (error) {
+      $scope.message = "Erro ao carregar contatos: " + error.data;
+    });
+  };
 
-    let carregarOperadoras = function () {
-        operadorasAPI.getOperadoras().then(function (response) {
-            $scope.operadoras = response.data;
-        });
-    };
+  let carregarOperadoras = function () {
+    operadorasAPI.getOperadoras().then(function (response) {
+      $scope.operadoras = response.data;
+    });
+  };
 
-    $scope.adicionarContato = function (contato) {        
-        contato.serial = serialGenerator.generate();
-        contato.data = new Date();
-        contatosAPI.saveContato(contato).then(function (repsonse) {
-            delete $scope.contato;
-            $scope.contatoForm.$setPristine();
-            carregarContatos();
-        });
-    };
-    $scope.estaSelecionado = function () {
-        return $scope.contatos.some(function (contato) {
-            return contato.selecionado;
-        });
-    };
-    $scope.apagarContatos = function (contatos) {
-        $scope.contatos = contatos.filter(function (contato) {
-            return !contato.selecionado;
-        });
-    };
-    $scope.ordenarPor = function (campo) {
-        $scope.criterioOrdenacao = campo;
-        $scope.direcaoOrdenacao = !$scope.direcaoOrdenacao;
-    };
+  $scope.adicionarContato = function (contato) {
+    contato.serial = serialGenerator.generate();
+    contato.data = new Date();
+    contatosAPI.saveContato(contato).then(function (response) {
+      delete $scope.contato;
+      $scope.contatoForm.$setPristine();
+      carregarContatos();
+    });
+  };
+  $scope.estaSelecionado = function () {
+    return $scope.contatos.some(function (contato) {
+      return contato.selecionado;
+    });
+  };
+  $scope.apagarContatos = function (contatos) {
+    $scope.contatos = contatos.filter(function (contato) {
+      return !contato.selecionado;
+    });
+  };
+  $scope.ordenarPor = function (campo) {
+    $scope.criterioOrdenacao = campo;
+    $scope.direcaoOrdenacao = !$scope.direcaoOrdenacao;
+  };
 
-    carregarContatos();
-    carregarOperadoras();
+  carregarContatos();
+  carregarOperadoras();
 });
