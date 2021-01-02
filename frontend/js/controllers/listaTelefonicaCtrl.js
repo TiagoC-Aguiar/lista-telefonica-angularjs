9e0622264c95da4b0960f5730616df63ddf0f301
Tiago Aguiar <tyago_tka48@hotmail.com>
2021-01-02 10:22:05 -0300

    correçao de bug no filter name

--- frontend/js/controllers/listaTelefonicaCtrl.js
+++ frontend/js/controllers/listaTelefonicaCtrl.js
@@ -26,7 +26,6 @@ angular.module('listaTelefonica').controller('listaTelefonicaCtrl', function ($s
 
   $scope.adicionarContato = function (contato) {
     contato.serial = serialGenerator.generate();
-    contato.data = new Date();
     contatosAPI.saveContato(contato).then(function (response) {
       delete $scope.contato;
       $scope.contatoForm.$setPristine();
