26da614b2765932515ec208cb66aec1336c972b1
Tiago Aguiar <tyago_tka48@hotmail.com>
2021-01-07 20:12:51 -0300

    criado interceptor para redirecionar erros 404 para pagina de erro

--- frontend/js/config/interceptorConfig.js
+++ frontend/js/config/interceptorConfig.js
@@ -1,3 +1,4 @@
 angular.module('listaTelefonica').config(function ($httpProvider) {
   $httpProvider.interceptors.push('timestampInterceptor');
+  $httpProvider.interceptors.push('errorInterceptor');
 });
